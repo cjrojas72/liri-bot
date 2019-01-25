71fede2c6e533dabea4aa61d2d0de69b2df231b3
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-01-25 12:22:31 -0500

    updating bot

--- liri.js
+++ liri.js
@@ -10,20 +10,45 @@ var axios = require("axios");
 
 var moment = require('moment');
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var action = process.argv[2];
+var userObj = process.argv[3];
 
-var movieName = process.argv[2];
+var queryUrl = "http://www.omdbapi.com/?t=" + userObj + "&y=&plot=short&apikey=trilogy";
 
-axios.get(queryUrl).then(
-  function(response){
-    console.log(response);
-  }
-)
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+console.log(queryUrl);
+
+
+switch(action){
+  case "concert-this":
+    break;
+
+  case "spotify-this-song":
+    spotify.search({ type: 'track', query: userObj }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log(data); 
+    });
+    break;
+  
+  case "movie-this":
+    axios.get(queryUrl).then(
+    function(response){
+      console.log("Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("Rating: " + response.data.Rated);
+      console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Rating: " + response.data.Rated);
+      console.log("Actors: " + response.data.Actors);
+    }
+    )
+    break;
+
+  case "do-what-it-says":
+    break;
+}
+
