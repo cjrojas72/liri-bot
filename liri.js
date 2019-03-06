32943f0c52042ee824409f078c51f4d26b0375bd
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-03-06 13:02:07 -0500

    fix to switch statements

--- liri.js
+++ liri.js
@@ -13,14 +13,20 @@ var moment = require('moment');
 var action = process.argv[2];
 var userObj = process.argv[3];
 
-var queryUrl = "http://www.omdbapi.com/?t=" + userObj + "&y=&plot=short&apikey=trilogy";
-
-
-console.log(queryUrl);
-
 
 switch(action){
   case "concert-this":
+  let queryUrl = `https://rest.bandsintown.com/artists/${userObj}/events?app_id=codingbootcamp`;
+  axios.get(queryUrl).then(
+    function(response) {
+      const data = response.data[0];
+      const time = data.datetime.replace("T", " ");
+      const convertedTime = moment(time, "YYYY-MM-DD HH:mm:ss").format("MMM Do, YYYY hh:mmA");
+      console.log(`Venue Name: ${data.venue.name}`);
+      console.log(`Venue Location: ${data.venue.city}, ${data.venue.region}`);
+      console.log(`Date: ${convertedTime}`);
+    });
+
     break;
 
   case "spotify-this-song":
@@ -28,12 +34,15 @@ switch(action){
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    console.log(data.tracks.items[0].artists); 
+    console.log("Artists: " + data.tracks.items[0].artists); 
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
     });
     break;
   
   case "movie-this":
-    axios.get(queryUrl).then(
+  let queryUrlm = `http://www.omdbapi.com/?t=${userObj}&y=&plot=short&apikey=trilogy`;
+    axios.get(queryUrlm).then(
     function(response){
       console.log("Title: " + response.data.Title);
       console.log("Year: " + response.data.Year);
